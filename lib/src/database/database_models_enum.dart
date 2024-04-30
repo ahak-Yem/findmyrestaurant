17c4aae7a628f7295e80e5cbe9d1a8198ca9b5ef
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-04-30 23:28:04 +0200

    Add enum for the models and boxes in Hive

---
+++ lib/src/database/database_models_enum.dart
@@ -0,0 +1,25 @@
+import 'package:hive/hive.dart';
+
+enum DatabaseModelsEnum {
+  userProfile,
+}
+
+extension AppBoxExtension on DatabaseModelsEnum {
+  String get boxName {
+    switch (this) {
+      case DatabaseModelsEnum.userProfile:
+        return 'userBox';
+      default:
+        throw Exception('Invalid enum value');
+    }
+  }
+
+  Future<Box<dynamic>> get box async {
+    switch (this) {
+      case DatabaseModelsEnum.userProfile:
+        return Hive.openBox(boxName);
+      default:
+        throw Exception('Invalid enum value');
+    }
+  }
+}
