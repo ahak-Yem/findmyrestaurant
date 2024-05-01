8fcf004e39307b3509481bb8ff785e50ee82820f
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-01 05:38:52 +0200

    Adjust main for testing

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   final settingsController = SettingsController(SettingsService());
-  // final userProfileController = UserProfileController();
   await settingsController.loadSettings();
   runApp(MyApp(settingsController: settingsController));
 }
