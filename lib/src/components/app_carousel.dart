943feb48ebb3683595a093b416f8b588e6d08e74
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-04 03:10:12 +0200

    Add a reusable app carousel component

---
+++ lib/src/components/app_carousel.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class AppCarousel extends StatefulWidget {
+  final List<Widget> items;
+  final double heightPercentage;
+  final Function(int) onPageChanged;
+
+  const AppCarousel({
+    Key? key,
+    required this.items,
+    required this.heightPercentage,
+    required this.onPageChanged,
+  }) : super(key: key);
+
+  @override
+  createState() => _AppCarouselState();
+}
+
+class _AppCarouselState extends State<AppCarousel> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height * widget.heightPercentage / 100,
+          child: PageView.builder(
+            itemCount: widget.items.length,
+            onPageChanged: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+              widget.onPageChanged(index);
+            },
+            itemBuilder: (context, index) {
+              return widget.items[index];
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
