a7714a66094369de392c10ce7394b7f80e7e952e
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-04 14:53:12 +0200

    Add a styles class for app colors

---
+++ lib/app styles/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF7391ff);
+  static const Color appWhite = Color(0xFFFFFFFF);
+  // Add more color constants as needed
+}
