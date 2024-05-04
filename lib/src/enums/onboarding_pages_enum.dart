f35ec3e9585e41269f0a936fb57f819b1e8b78d8
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-04 14:53:52 +0200

    Pass the app dynamic button and use the app colors.

--- lib/src/enums/onboarding_pages_enum.dart
+++ lib/src/enums/onboarding_pages_enum.dart
@@ -1,6 +1,8 @@
 import 'package:findmyrestaurant/src/app_carousel_controller.dart';
+import 'package:findmyrestaurant/src/components/app%20buttons/app_dynamic_button.dart';
 import 'package:findmyrestaurant/src/items/app_carousel_item.dart';
 import 'package:flutter/material.dart';
+import 'package:findmyrestaurant/app styles/app_colors.dart';
 
 enum OnboardingPages {
   intro(
@@ -22,12 +24,14 @@ extension OnboardingPagesExtension on OnboardingPages{
         headerText: 'Howdy & Welcome to Find my Restaurant App',
         subText: "Here you will find a brief overview of the app and how to use it's wonderful features.",
         buttons: [
-          ElevatedButton(
-            onPressed: () {
+          AppDynamicButton(
+            color: AppColors.primaryColor,
+            textColor: AppColors.appWhite,
+            text: 'Get Started',
+            onPressed: (){
               _carouselController.animateToPage(pageIndex + 1);
-            },
-            child: const Text('Get Started'),
-          ),
+            }
+          )
         ],
       );
     }
