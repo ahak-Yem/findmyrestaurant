7ba0491b590dcb45851821dac8470fa597daf26c
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-06 01:46:07 +0200

    Use the AppCarousel controller in the AppCarousel and set it for the Pages

--- lib/src/components/app_carousel.dart
+++ lib/src/components/app_carousel.dart
@@ -1,15 +1,19 @@
 import 'package:findmyrestaurant/src/components/dot_page_indicator.dart';
+import 'package:findmyrestaurant/src/controllers/app_carousel_controller.dart';
+import 'package:findmyrestaurant/src/enums/onboarding_pages_enum.dart';
 import 'package:findmyrestaurant/src/items/app_carousel_item.dart';
 import 'package:flutter/material.dart';
 
 class AppCarousel extends StatefulWidget {
   final List<AppCarouselItem> items;
+  final PageController pageController;
   final double heightPercentage;
   final Function(int) onPageChanged;
 
   const AppCarousel({
     Key? key,
     required this.items,
+    required this.pageController,
     required this.heightPercentage,
     required this.onPageChanged,
   }) : super(key: key);
@@ -22,12 +26,21 @@ class _AppCarouselState extends State<AppCarousel> {
   int _currentIndex = 0;
 
   @override
+  void initState() {
+    super.initState();
+    if(OnboardingPagesExtension.carouselController == null && widget.pageController is AppCarouselController){
+      OnboardingPagesExtension.setAppCarouselController(widget.pageController as AppCarouselController);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         SizedBox(
           height: MediaQuery.of(context).size.height * widget.heightPercentage / 100,
           child: PageView.builder(
+            controller: widget.pageController,
             itemCount: widget.items.length,
             onPageChanged: (index) {
               setState(() {
