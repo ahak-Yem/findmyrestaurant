98108a7aca8f5195dc1ae1b49f6f07a1aeaebb88
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-06 01:43:31 +0200

    Refactor AppCarouselController to be used from somewhere other than the AppCarousel

--- lib/src/controllers/app_carousel_controller.dart
+++ lib/src/controllers/app_carousel_controller.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 
-class AppCarouselController<T> extends ChangeNotifier {
-  final PageController _pageController = PageController();
-
-  int get currentPage => _pageController.page?.round() ?? 0;
-
-  void animateToPage(int page, {Duration duration = const Duration(milliseconds: 300), Curve curve = Curves.ease}) {
-    _pageController.animateToPage(
-      page,
-      duration: duration,
-      curve: curve,
-    );
-  }
+class AppCarouselController extends PageController {
+  AppCarouselController({
+    int initialPage = 0,
+    double viewportFraction = 1.0,
+    bool keepPage = true,
+  }) : super(
+          initialPage: initialPage,
+          viewportFraction: viewportFraction,
+          keepPage: keepPage,
+        );
 
   @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
+  Future<void> animateToPage(int page, {Duration duration = const Duration(milliseconds: 300), Curve curve = Curves.ease}) {
+    if (hasClients) {
+      return super.animateToPage(
+        page,
+        curve: curve,
+        duration: duration,
+      );
+    }
+    return Future(() => null);
   }
 }
