7bb7c1f389f24f43f2a2e14fe9fc1413e5e9cc94
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-05 22:00:42 +0200

    Add a dot page indicator components in the app components

---
+++ lib/src/components/dot_page_indicator.dart
@@ -0,0 +1,35 @@
+import 'package:findmyrestaurant/app%20styles/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class PageIndicator extends StatelessWidget {
+  final int currentPage;
+  final int pageCount;
+  final Color activeDotColor;
+  final Color inactiveDotColor;
+
+  const PageIndicator({
+    Key? key,
+    required this.currentPage,
+    required this.pageCount,
+    this.activeDotColor = AppColors.darkGray,
+    this.inactiveDotColor = AppColors.gray,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(pageCount, (index) {
+        return Container(
+          width: 8,
+          height: 8,
+          margin: const EdgeInsets.symmetric(horizontal: 4),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: index == currentPage ? activeDotColor : inactiveDotColor,
+          ),
+        );
+      }),
+    );
+  }
+}
