ae268f7476e75ad7fd8a6ddf3a2cf984f6073db8
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-05 21:11:56 +0200

    Add a second OnBoardingPage/CarouselItem for testing in `OnboardingPages`

--- lib/src/enums/onboarding_pages_enum.dart
+++ lib/src/enums/onboarding_pages_enum.dart
@@ -7,6 +7,9 @@ import 'package:findmyrestaurant/app styles/app_colors.dart';
 enum OnboardingPages {
   intro(
     pageIndex: 0
+  ),
+  secondTestView(
+    pageIndex: 1
   );
 
   const OnboardingPages({required this.pageIndex});
@@ -14,40 +17,57 @@ enum OnboardingPages {
   final int pageIndex;
 }
 
-extension OnboardingPagesExtension on OnboardingPages{
+extension OnboardingPagesExtension on OnboardingPages {
   static final AppCarouselController<int> _carouselController = AppCarouselController<int>();
 
-  AppCarouselItem get page{
-    switch(this){
+  AppCarouselItem get page {
+    switch(this) {
       case OnboardingPages.intro: 
-      return AppCarouselItem(
-        headerText: AppStrings.welcomeMessage,
-        subText: AppStrings.welcomeSubMessage,
-        buttons: [
-          AppDynamicButton(
-            color: AppColors.primaryColor,
-            textColor: AppColors.appWhite,
-            text: AppStrings.getStartedBtn,
-            onPressed: (){
-              _carouselController.animateToPage(pageIndex + 1);
-            }
-          )
-        ],
-      );
+        return AppCarouselItem(
+          headerText: AppStrings.welcomeMessage,
+          subText: AppStrings.welcomeSubMessage,
+          buttons: [
+            AppDynamicButton(
+              color: AppColors.primaryColor,
+              textColor: AppColors.appWhite,
+              text: AppStrings.getStartedBtn,
+              onPressed: () {
+                _carouselController.animateToPage(pageIndex + 1);
+              }
+            )
+          ],
+        );
+      case OnboardingPages.secondTestView: 
+        return AppCarouselItem(
+          headerText: "Second Test View Header",
+          subText: "This is the second test view subtext.",
+          buttons: [
+            AppDynamicButton(
+              color: AppColors.primaryColor,
+              textColor: AppColors.appWhite,
+              text: "Continue",
+              onPressed: () {
+                _carouselController.animateToPage(pageIndex + 1);
+              }
+            )
+          ],
+        );
     }
   }
 
   static AppCarouselItem getPageByIndex(int index) {
     AppCarouselItem emptyItem = const AppCarouselItem(
-        headerText: "",
-        subText: "",
-      );
-    if (OnboardingPages.values.any((page) => page.pageIndex != index)) {
-      return emptyItem; 
+      headerText: "",
+      subText: "",
+    );
+    if (!OnboardingPages.values.any((page) => page.pageIndex == index)) {
+      return emptyItem;
     }
     switch(index) {
       case 0:
         return OnboardingPages.intro.page;
+      case 1:
+        return OnboardingPages.secondTestView.page;
       default:
         return emptyItem;
     }
