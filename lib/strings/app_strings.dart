799c362484285e418bb21eb58ac7e55d4565c1ac
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-06 22:36:56 +0200

    Change var name from goBack to goBackBtn in app strings

--- lib/strings/app_strings.dart
+++ lib/strings/app_strings.dart
@@ -3,7 +3,7 @@ class AppStrings {
   static const String welcomeMessage = "Welcome to Find my Restaurant!";
   static const String welcomeSubMessage = "Here you will find a brief overview of the app and how to use it's wonderful features.";
   static const String getStartedBtn = "Get Started";
-  static const String goBack = "Go Back";
+  static const String goBackBtn = "Go Back";
 
   static const String errorTitle = 'Error';
   static const String errorMessage = 'An error occurred. Please try again later.';
