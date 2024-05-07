5e63ce4d7013483b65a37958faac1beb6de9d665
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-07 22:35:15 +0200

    Remove unused imports in the `OnboardingPages` enum

--- lib/src/enums/onboarding_pages_enum.dart
+++ lib/src/enums/onboarding_pages_enum.dart
@@ -1,10 +1,7 @@
 import 'package:findmyrestaurant/src/pages/onboarding_carousel_pages/getstarted_page.dart';
 import 'package:findmyrestaurant/src/pages/onboarding_carousel_pages/intro_page.dart';
-import 'package:findmyrestaurant/strings/app_strings.dart';
 import 'package:findmyrestaurant/src/controllers/app_carousel_controller.dart';
-import 'package:findmyrestaurant/src/components/app%20buttons/app_dynamic_button.dart';
 import 'package:findmyrestaurant/src/items_templates/app_carousel_item.dart';
-import 'package:findmyrestaurant/styles/app_colors.dart';
 
 enum OnboardingPages {
   intro(
