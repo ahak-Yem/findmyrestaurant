87600d6955378866bdbe16398589150b1f0b10ab
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-08 02:35:50 +0200

    Adjust app carousel item template to fit for all pages so far

--- lib/src/items_templates/app_carousel_item.dart
+++ lib/src/items_templates/app_carousel_item.dart
@@ -1,6 +1,8 @@
+import 'package:findmyrestaurant/src/enums/onboarding_pages_enum.dart';
 import 'package:flutter/material.dart';
 
 class AppCarouselItem extends StatelessWidget {
+  final OnboardingPages page;
   final String? headerText;
   final String? subText;
   final List<Widget>? buttons;
@@ -8,6 +10,7 @@ class AppCarouselItem extends StatelessWidget {
 
   const AppCarouselItem({
     Key? key,
+    required this.page,
     this.headerText,
     this.subText,
     this.buttons,
@@ -16,42 +19,53 @@ class AppCarouselItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-          if (headerText != null && headerText!.isNotEmpty) ...[  
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8, 4, 8, 12),
-              child: Text(
-                headerText!,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+    return ListView(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              if(page != OnboardingPages.signUp)...
+              [
+                SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+              ],
+              if (headerText != null && headerText!.isNotEmpty) ...[  
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8, 4, 8, 12),
+                  child: Text(
+                    headerText!,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ],
-          if (subText != null && subText!.isNotEmpty) ...[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8, 2, 8, 24),
-              child: Text(
-                subText!,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 14,
+              ],
+              if (subText != null && subText!.isNotEmpty) ...[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8, 2, 8, 24),
+                  child: Text(
+                    subText!,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ],
-          if (textFields != null) ...textFields!,
-          const SizedBox(height: 16),
-          if (buttons != null) ...buttons!,
-        ],
-      ),
+              ],
+              if (textFields != null) ...textFields!,
+              const SizedBox(height: 16),
+              if (buttons != null) ...buttons!,
+            ],
+          ),
+        ),
+        if(page == OnboardingPages.signUp)...
+        [
+          SizedBox(height: MediaQuery.of(context).size.height * 0.3,),
+        ]
+      ],
     );
   }
 }
