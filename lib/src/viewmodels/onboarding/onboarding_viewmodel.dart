87bc01f021bd573c6fc8cbc48f92f61bdd871a69
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-10 04:51:47 +0200

    Refactor viewmodel page to check and validate if the the entered values in the text fields of the sign up page are correct

--- lib/src/viewmodels/onboarding/onboarding_viewmodel.dart
+++ lib/src/viewmodels/onboarding/onboarding_viewmodel.dart
@@ -2,6 +2,7 @@ import 'package:findmyrestaurant/src/controllers/app_carousel_controller.dart';
 import 'package:findmyrestaurant/src/enums/images%20enums/images_names.dart';
 import 'package:findmyrestaurant/src/enums/onboarding_pages_enum.dart';
 import 'package:findmyrestaurant/src/items_templates/app_carousel_item.dart';
+import 'package:findmyrestaurant/src/pages/onboarding_carousel_pages/signup_page.dart';
 import 'package:flutter/foundation.dart';
 import 'package:findmyrestaurant/src/services/images_reader_service.dart';
 import 'package:findmyrestaurant/src/enums/images%20enums/images_paths_sections_enum.dart';
@@ -40,6 +41,9 @@ class OnboardingViewModel extends ChangeNotifier {
   void onAppCarouselItemChanged(int pageIndex){
     int imageIndex = pageIndex % imageNames.length;
     designImagePath = _getDesignImagePath(imageNames[imageIndex]);
+    if(OnboardingPages.signUp.pageIndex + 1 == pageIndex){
+      _validateSignUp();
+    }
     notifyListeners();
   }
 
@@ -49,5 +53,21 @@ class OnboardingViewModel extends ChangeNotifier {
       carouselItems.add(page.page);
     }
     notifyListeners();
+  }
+
+  Map<bool, String> _validateSignUp(){
+    String name = SignupPage.nameController.text;
+    String email = SignupPage.emailController.text;
+    String password = SignupPage.passwordController.text;
+    String confirmPassword = SignupPage.confirmPasswordController.text;
+
+    final Map<bool, String> validationResult = appCarouselController.validateSignup(
+      name: name,
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+    );
+
+    return validationResult;
   }
 }
