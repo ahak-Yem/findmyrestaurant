3151c21eea51e7c5cce0cd4e13d06b60949362d3
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-16 21:45:24 +0200

    Initialized DotenvService in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:findmyrestaurant/src/services/database/database_service.dart';
+import 'package:findmyrestaurant/src/services/dotenv_service.dart';
 import 'package:findmyrestaurant/src/services/images_reader_service.dart';
 import 'package:flutter/material.dart';
 import 'src/app.dart';
@@ -6,6 +7,7 @@ import 'src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await DotenvService.instance.initialize();
   await DatabaseService.instance.initialize();
   await ImagesReaderService.instance.initialize();
   runApp(const MyApp());
