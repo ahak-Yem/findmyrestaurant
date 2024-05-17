e423dc9b82761f1a8c0906ae816d94ef68a4cbe7
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-17 15:42:41 +0200

    Add confirmation code property and getter and refactor class for readability

--- lib/src/services/email_service.dart
+++ lib/src/services/email_service.dart
@@ -12,25 +12,41 @@ class EmailService {
   final DotenvService _dotenvService = DotenvService.instance; 
   
   String? _htmlText; 
+  
+  String _confirmationCode = "";
+  String get confirmationCode => _confirmationCode;
+  bool isEmailReady = false;
 
   Future<void> initialize() async{
-    _htmlText = await _fetchLocalEmailTemplate();
+    _htmlText = await _setConfirmEmailTemplate();
   }
 
-  Future<String> _fetchLocalEmailTemplate() async{
+  Future<String> _setConfirmEmailTemplate() async{
     const String htmlFilePath = "assets/html pages/confirm_email.html";
-    String templateText = _injectConfirmationCodeInHtml(await rootBundle.loadString(htmlFilePath));
+    String templateText = "";
+    try{
+      String htmlOrig = await rootBundle.loadString(htmlFilePath);
+      templateText = _injectConfirmationCodeInHtml(htmlOrig);
+      isEmailReady = true;
+    }
+    catch(e){
+      isEmailReady = false;
+    }
     return templateText;
   }
 
-  String _injectConfirmationCodeInHtml(String htmlString, {final int codeLength = 6}) {
-    const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    String confirmationCode = List.generate(codeLength, (index) => chars[Random().nextInt(chars.length)]).join();
+  String _injectConfirmationCodeInHtml(String htmlString,) {
+    _setConfiramtionCode();
     final RegExp regex = RegExp(r'<p id="confirmationCode" class="default-button"></p>');
     String htmlContent = htmlString.replaceAllMapped(regex, (match) {
       return '<p id="confirmationCode" class="default-button">$confirmationCode</p>';
     });
     return htmlContent;
+  }
+  
+  void _setConfiramtionCode({final int codeLength = 6}){
+    const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    _confirmationCode = List.generate(codeLength, (index) => chars[Random().nextInt(chars.length)]).join();
   }
   
   Future<bool> sendOTP(String userEmail) async {
