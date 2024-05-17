ba688d29f3f5a9cc673f95c809825993355f883a
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-18 00:37:23 +0200

    Add isSuccess getter and mapCodeToBrevoApiResponses function
    - Added a getter isSuccess to BrevoApiResponsesExtension to determine if a response is successful based on the value of the enum BrevoApiResponses.
    - Implemented a function mapCodeToBrevoApiResponses to map status codes to BrevoApiResponses enum values, facilitating easier handling of HTTP response codes.

--- lib/src/enums/brevo_api_responses_enum.dart
+++ lib/src/enums/brevo_api_responses_enum.dart
@@ -43,5 +43,43 @@ extension BrevoApiResponsesExtension on BrevoApiResponses{
     }
   }
 
+  bool get isSuccess {
+    return this == BrevoApiResponses.ok ||
+        this == BrevoApiResponses.created ||
+        this == BrevoApiResponses.accepted ||
+        this == BrevoApiResponses.noContent;
+  }
+
+  static BrevoApiResponses mapCodeToBrevoApiResponses(int statusCode) {
+    switch (statusCode) {
+      case 200:
+        return BrevoApiResponses.ok;
+      case 201:
+        return BrevoApiResponses.created;
+      case 202:
+        return BrevoApiResponses.accepted;
+      case 204:
+        return BrevoApiResponses.noContent;
+      case 400:
+        return BrevoApiResponses.badRequest;
+      case 401:
+        return BrevoApiResponses.unauthorized;
+      case 402:
+        return BrevoApiResponses.paymentRequired;
+      case 403:
+        return BrevoApiResponses.forbidden;
+      case 404:
+        return BrevoApiResponses.notFound;
+      case 405:
+        return BrevoApiResponses.methodNotAllowed;
+      case 406:
+        return BrevoApiResponses.notAcceptable;
+      case 429:
+        return BrevoApiResponses.tooManyRequests;
+      default:
+        return BrevoApiResponses.badRequest;
+    }
+  }
+
   // TODO: Create a getter that returns handling workflow
 }
