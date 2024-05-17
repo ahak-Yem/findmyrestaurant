20381363897e53193045f468645834f39f1b6ab2
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-17 15:17:00 +0200

    Fetch html file for email confirmation and inject confirmation code in it.

--- lib/src/services/email_service.dart
+++ lib/src/services/email_service.dart
@@ -1,4 +1,7 @@
 import 'package:findmyrestaurant/src/services/dotenv_service.dart';
+import 'dart:math';
+import 'package:flutter/services.dart';
+
 class EmailService {
   EmailService._();
 
@@ -11,6 +14,28 @@ class EmailService {
   final DotenvService _dotenvService = DotenvService.instance; 
   
   bool _isConfigured = false;
+
+  String? _htmlText; 
+
+  Future<void> initialize() async{
+    _htmlText = await _fetchLocalEmailTemplate();
+  }
+
+  Future<String> _fetchLocalEmailTemplate() async{
+    const String htmlFilePath = "assets/html pages/confirm_email.html";
+    String templateText = _injectConfirmationCodeInHtml(await rootBundle.loadString(htmlFilePath));
+    return templateText;
+  }
+
+  String _injectConfirmationCodeInHtml(String htmlString, {final int codeLength = 6}) {
+    const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    String confirmationCode = List.generate(codeLength, (index) => chars[Random().nextInt(chars.length)]).join();
+    final RegExp regex = RegExp(r'<p id="confirmationCode" class="default-button"></p>');
+    String htmlContent = htmlString.replaceAllMapped(regex, (match) {
+      return '<p id="confirmationCode" class="default-button">$confirmationCode</p>';
+    });
+    return htmlContent;
+  }
   
   Future<bool> sendOTP(String userEmail) async {
     bool isSent = false;
