81e15c92c33060e14ed09f05a63c4ee07d5655bc
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-17 17:55:29 +0200

    Remove SMTP-related keys from DotenvKeys enum and corresponding keyString values.

--- lib/src/enums/dotenv_keys_enum.dart
+++ lib/src/enums/dotenv_keys_enum.dart
@@ -1,19 +1,10 @@
 enum DotenvKeys {
-  smtpHost,
-  smtpUser,
-  smtpPassword,
   brevoApi;
 }
 
 extension EnvKeysExtension on DotenvKeys {
   String get keyString {
     switch (this) {
-      case DotenvKeys.smtpHost:
-        return 'SMTP_HOST';
-      case DotenvKeys.smtpUser:
-        return 'SMTP_USER';
-      case DotenvKeys.smtpPassword:
-        return 'SMTP_PASSWORD';
       case DotenvKeys.brevoApi:
         return 'BREVO_API';
     }
