211a958f5c5dcb88bc7bae650e3ba3bf2464f450
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-17 22:42:44 +0200

    Adjust the email model to have list of receivers, make tags optional and add toJSON object converter util function

--- lib/src/models/email_model.dart
+++ lib/src/models/email_model.dart
@@ -2,7 +2,7 @@ class EmailModel {
   
   ContactModel sender;
 
-  ContactModel receiver;
+  List<ContactModel> receivers;
 
   ContactModel? replyTo;
 
@@ -12,7 +12,7 @@ class EmailModel {
 
   String? previewText;
 
-  List<String> tags;
+  List<String>? tags;
 
   String? cc;
   
@@ -20,15 +20,42 @@ class EmailModel {
 
   EmailModel({
     required this.sender,
-    required this.receiver,
+    required this.receivers,
     this.replyTo,
     required this.emailHtml,
     required this.subject,
     this.previewText,
-    this.tags = const [],
+    this.tags,
     this.cc,
     this.bcc,
   });
+
+  Map<String, dynamic> toJSON() {
+    final Map<String, dynamic> data = {};
+
+    data['sender'] = sender.toJSON();
+    for (var receiver in receivers) { 
+      data['receiver'] = receiver.toJSON();
+    }
+    if (replyTo != null) {
+      data['replyTo'] = replyTo?.toJSON();
+    }
+    data['emailHtml'] = emailHtml;
+    data['subject'] = subject;
+    if (previewText != null) {
+      data['previewText'] = previewText;
+    }
+    if (tags != null && tags!.isNotEmpty) {
+      data['tags'] = tags;
+    }
+    if (cc != null) {
+      data['cc'] = cc;
+    }
+    if (bcc != null) {
+      data['bcc'] = bcc;
+    }
+    return data;
+  }
 } 
 
 class ContactModel {
@@ -36,4 +63,13 @@ class ContactModel {
   String email;
 
   ContactModel({this.name, required this.email});
+
+  Map<String, String> toJSON() {
+    final Map<String, String> data = {};
+    if (name != null) {
+      data['name'] = name!;
+    }
+    data['email'] = email;
+    return data;
+  }
 }
