ce4b8aa49ab8ad70a8f97bb075c2f89679ee57dd
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-17 19:40:28 +0200

    Adjust the email service to use the html service instead of local codes

--- lib/src/services/email_service.dart
+++ lib/src/services/email_service.dart
@@ -1,7 +1,8 @@
 import 'package:findmyrestaurant/src/enums/dotenv_keys_enum.dart';
+import 'package:findmyrestaurant/src/enums/html_files_enum.dart';
 import 'package:findmyrestaurant/src/services/confirmation_code_service.dart';
 import 'package:findmyrestaurant/src/services/dotenv_service.dart';
-import 'package:flutter/services.dart';
+import 'package:findmyrestaurant/src/services/html_service.dart';
 
 class EmailService {
   // Singleton Section
@@ -10,12 +11,17 @@ class EmailService {
   factory EmailService() => _instance;
   static EmailService get instance => _instance;
 
+  // Services
   final DotenvService _dotenvService = DotenvService.instance;
   final ConfirmationCodeService confirmationCodeService = ConfirmationCodeService();
+  final HtmlService _htmlService = HtmlService.instance;
   
+  // Confirmation Email Variables
   String? _confirmationEmailHtml; 
   String _confirmationCode = "";
   bool isEmailReady = false;
+
+  // Brevo API Variables
   final String _brevoApiEndpoint = "https://api.brevo.com/v3/";
   String? _brevoApiKey;
 
@@ -25,10 +31,10 @@ class EmailService {
   }
 
   Future<String> _setConfirmEmailTemplate() async{
-    const String htmlFilePath = "assets/html pages/confirm_email.html";
+    const HtmlFiles htmlFile = HtmlFiles.confirmationEmail;
     String templateText = "";
     try{
-      String htmlOrig = await rootBundle.loadString(htmlFilePath);
+      String htmlOrig = await _htmlService.fetchHtml(htmlFile);
       templateText = _injectConfirmationCodeInHtml(htmlOrig);
       isEmailReady = true;
     }
@@ -40,10 +46,9 @@ class EmailService {
 
   String _injectConfirmationCodeInHtml(String htmlString,) {
     _setConfiramtionCode();
-    final RegExp regex = RegExp(r'<p id="confirmationCode" class="default-button"></p>');
-    String htmlContent = htmlString.replaceAllMapped(regex, (match) {
-      return '<p id="confirmationCode" class="default-button">$_confirmationCode</p>';
-    });
+    const String oldHtmlStr = '<p id="confirmationCode" class="default-button"></p>';
+    String newHtmlStr = '<p id="confirmationCode" class="default-button">$_confirmationCode</p>';
+    String htmlContent = _htmlService.injectInHtml(htmlCode: htmlString, oldHtmlString: oldHtmlStr, newHtmlString: newHtmlStr);
     return htmlContent;
   }
   
