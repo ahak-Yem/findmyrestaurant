0acc474398314079b3cb5266949b65d7ad7a831b
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-22 23:28:05 +0200

    Add an app color red

--- lib/styles/app_colors.dart
+++ lib/styles/app_colors.dart
@@ -5,4 +5,5 @@ class AppColors {
   static const Color appWhite = Color(0xFFFFFFFF);
   static const Color darkGray = Color.fromARGB(255, 59, 59, 59);
   static const Color gray = Colors.grey;
+  static const Color red = Color.fromARGB(255, 255, 0, 34);
 }
