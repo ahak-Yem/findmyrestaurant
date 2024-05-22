1b052de1721e801b801b33473befc0efb8ade47e
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-22 15:29:14 +0200

    Add new confirm email page with CodeBoxesTextField

---
+++ lib/src/pages/onboarding_carousel_pages/confirm_email_page.dart
@@ -0,0 +1,58 @@
+import 'package:findmyrestaurant/src/components/app%20buttons/app_dynamic_button.dart';
+import 'package:findmyrestaurant/src/components/text%20fields/code_boxes_text_field.dart';
+import 'package:findmyrestaurant/src/controllers/app_carousel_controller.dart';
+import 'package:findmyrestaurant/src/enums/onboarding_pages_enum.dart';
+import 'package:findmyrestaurant/src/items_templates/app_carousel_item.dart';
+import 'package:findmyrestaurant/strings/app_strings.dart';
+import 'package:findmyrestaurant/styles/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class ConfirmEmailPage {
+  ConfirmEmailPage._();
+
+  static final AppCarouselController? appCarouselController = OnboardingPagesExtension.carouselController;
+  static final TextEditingController confirmationCodeController = TextEditingController();
+  static List<String> textCharacters = List.filled(6, '');
+
+  static AppCarouselItem get page {
+    const boxesAmount = 6;
+    return AppCarouselItem(
+      page: OnboardingPages.confirmEmail,
+      headerText: AppStrings.confirmationEmailHeader,
+      subText: AppStrings.confirmationEmailSubtext,
+      textFields: [
+        CodeBoxesTextField(
+          onChanged: (value, index) {
+            if (index < textCharacters.length && index < boxesAmount) {
+              textCharacters[index] = value;
+            }
+            confirmationCodeController.text = textCharacters.join();
+            if(confirmationCodeController.text.length == boxesAmount) {
+              //TODO: Verify code before going to next page :)
+              appCarouselController?.goNext(currentPage: OnboardingPages.confirmEmail.pageIndex);
+            }
+          },
+          boxesAmount: boxesAmount,
+        ),
+      ],
+      buttons: [
+        AppDynamicButton(
+          color: AppColors.primaryColor,
+          textColor: AppColors.appWhite,
+          text: AppStrings.resendBtn,
+          onPressed: () {
+            // TODO:Implement resending code logic
+          },
+        ),
+        AppDynamicButton(
+          color: AppColors.appWhite,
+          textColor: AppColors.primaryColor,
+          text: AppStrings.goBackBtn,
+          onPressed: () {
+            appCarouselController?.goBack(currentPage: OnboardingPages.confirmEmail.pageIndex);
+          },
+        ),
+      ],
+    );
+  }
+}
