7878ffa78521aae0a9d56bcfea8f91279299a297
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-22 22:25:27 +0200

    Adjust `ConfirmationCoderService` for verify code

--- lib/src/services/confirmation_code_service.dart
+++ lib/src/services/confirmation_code_service.dart
@@ -2,30 +2,30 @@ import 'dart:math';
 
 class ConfirmationCodeService {
   final int _codeLength = 6;
-  final Duration codeValidityDuration;
+  final Duration _codeValidityDuration = const Duration(minutes: 15);
 
   final Map<String, DateTime> _codes = {};
 
-  ConfirmationCodeService({this.codeValidityDuration = const Duration(minutes: 15)});
+  ConfirmationCodeService();
 
   String generateCode() {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     final code = List.generate(_codeLength, (index) => chars[Random().nextInt(chars.length)]).join();
     
-    _codes[code] = DateTime.now().add(codeValidityDuration);
+    _codes[code] = DateTime.now().add(_codeValidityDuration);
     return code;
   }
 
-  bool verifyCode(String code) {
+  Map<bool, DateTime?> verifyCode(String code) {
     final expirationTime = _codes[code];
     if (expirationTime == null) {
-      return false; 
+      return {false: expirationTime}; 
     }
     if (DateTime.now().isAfter(expirationTime)) {
       _codes.remove(code);
-      return false;
+      return {false: expirationTime};
     }
     _codes.remove(code);
-    return true;
+    return {true: expirationTime};
   }
 }
