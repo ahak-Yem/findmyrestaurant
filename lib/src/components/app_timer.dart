f5d09df8aaf7cc79fefc43a53de916820fe04855
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-24 00:51:07 +0200

    Adjust the app timer component to use the timer service to be manipulable

--- lib/src/components/app_timer.dart
+++ lib/src/components/app_timer.dart
@@ -1,61 +1,22 @@
-import 'dart:async';
-import 'package:findmyrestaurant/strings/app_strings.dart';
-import 'package:findmyrestaurant/styles/app_colors.dart';
+import 'package:findmyrestaurant/src/services/timer_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class TimerWidget extends StatefulWidget {
-  final int initialSeconds;
-  final VoidCallback? onTimerComplete;
+class TimerWidget extends StatelessWidget {
+  const TimerWidget({super.key, required this.text, required this.textColor});
 
-  const TimerWidget({Key? key, required this.initialSeconds, this.onTimerComplete}) : super(key: key);
-
-  @override
-  createState() => _TimerWidgetState();
-}
-
-class _TimerWidgetState extends State<TimerWidget> {
-  late int _remainingSeconds;
-  late Timer _timer;
-
-  @override
-  void initState() {
-    super.initState();
-    _startTimer();
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
-
-  void _startTimer() {
-    _remainingSeconds = widget.initialSeconds;
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        if (_remainingSeconds > 0) {
-          _remainingSeconds--;
-        } else {
-          timer.cancel();
-          if(widget.onTimerComplete != null){
-            widget.onTimerComplete;
-          }
-        }
-      });
-    });
-  }
-
-  String get _formattedTime {
-    int minutes = _remainingSeconds ~/ 60;
-    int seconds = _remainingSeconds % 60;
-    return '${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
-  }
+  final String text;
+  final Color textColor;
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      AppStrings.codeExpiresInLabel + _formattedTime,
-      style: const TextStyle(fontSize: 16, color: AppColors.red),
+    return Consumer<TimerService>(
+      builder: (context, timerService, child) {
+        return Text(
+          '$text ${timerService.formattedTime}',
+          style: TextStyle(fontSize: 16, color: textColor),
+        );
+      },
     );
   }
 }
