7e73f9bed8e773af52278d36d8a1a4c9a2ea16d3
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-24 03:43:24 +0200

    Update CompleteOnboardingPage subtext

--- lib/src/pages/onboarding_carousel_pages/complete_onboarding_page.dart
+++ lib/src/pages/onboarding_carousel_pages/complete_onboarding_page.dart
@@ -14,6 +14,7 @@ class CompleteOnboardingPage {
       subText: 
       '''
         ${AppStrings.confirmationEmailSubtext}
+        
         ${AppStrings.completeSignupConfirmationMsg}
       ''' ,
       buttons: [
