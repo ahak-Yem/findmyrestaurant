4c167bcdfd80ec54bf535f92577a0a6b9251b5a2
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-05-24 03:46:43 +0200

    Change completeSignupHeader in app strings

--- lib/strings/app_strings.dart
+++ lib/strings/app_strings.dart
@@ -32,7 +32,7 @@ class AppStrings {
   static const String expiredCodeErrorText = "The entered code is expired.";
   static const String codeSuccessfulText = "Nice, you confirmed your email successfully!";
   static const String codeExpiresInLabel = "Code expires in ";
-  static const String completeSignupHeader = "You have signed up successfully with us";
+  static const String completeSignupHeader = "You signed up successfully";
   static const String completeSignupSubtext = "Click the button below to be navigated to our dietary survey.";
   static const String completeSignupConfirmationMsg = "Your signup data are now saved in your device and you will not need to do the signup process again.";
   static const String toSurveyBtn = "Open Survey";
