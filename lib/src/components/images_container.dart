dbabb666c40c871bd8a18948dd41805f7af20e18
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-06-02 12:41:31 +0200

    Added imports, modified ImagesContainer widget to use AnimatedOpacity and frameBuilder for image loading, and introduced _isLoaded state variable.

--- lib/src/components/images_container.dart
+++ lib/src/components/images_container.dart
@@ -1,10 +1,14 @@
+import 'package:findmyrestaurant/src/enums/images%20enums/images_names.dart';
+import 'package:findmyrestaurant/src/enums/images%20enums/images_paths_sections_enum.dart';
+import 'package:findmyrestaurant/src/services/images_reader_service.dart';
 import 'package:flutter/material.dart';
 
 class ImagesContainer extends StatefulWidget {
   final String imagePath;
   final double heightPercentage;
 
-  const ImagesContainer({super.key, 
+  const ImagesContainer({
+    super.key,
     required this.imagePath,
     required this.heightPercentage,
   });
@@ -14,15 +18,58 @@ class ImagesContainer extends StatefulWidget {
 }
 
 class _ImagesContainerState extends State<ImagesContainer> {
+  bool _isLoaded = false;
+  late String _imagePlaceholder;
+
+  set isLoaded(bool isLoaded) {
+    setState(() {
+      _isLoaded = isLoaded;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _imagePlaceholder = ImagesReaderService.instance.getImagePath(
+      ImagesPathsSections.extra,
+      ImagesNames.loadingPlaceholder,
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: MediaQuery.of(context).size.height * widget.heightPercentage / 100,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(widget.imagePath),
-          fit: BoxFit.cover,
-        ),
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              _imagePlaceholder,
+              fit: BoxFit.contain,
+            ),
+          ),
+          Positioned.fill(
+            child: AnimatedOpacity(
+              opacity: _isLoaded ? 1.0 : 0.0,
+              duration: const Duration(milliseconds: 3000),
+              child: Image.asset(
+                widget.imagePath,
+                fit: BoxFit.cover,
+                frameBuilder: (BuildContext context, Widget child, int? frame, bool wasSynchronouslyLoaded) {
+                  if (wasSynchronouslyLoaded || frame != null) {
+                    Future.delayed(Duration.zero, () {
+                      setState(() {
+                        _isLoaded = true;
+                      });
+                    });
+                    return child;
+                  } else {
+                    return child;
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
