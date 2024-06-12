abbe8775fd124307619ae1bd2b636a8e943f5035
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-06-12 22:08:11 +0200

    Added dietaryUserPreferences to DatabaseModelsEnum and updated related extensions and methods.

--- lib/src/enums/database enums/database_models_enum.dart
+++ lib/src/enums/database enums/database_models_enum.dart
@@ -1,8 +1,10 @@
+import 'package:findmyrestaurant/src/enums/database%20enums/dietary_survey_user_preferences_keys_enum.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'user_model_keys_enum.dart';
 
 enum DatabaseModelsEnum {
   user,
+  dietaryUserPreferences,
 }
 
 extension DatabaseModelsEnumExtension on DatabaseModelsEnum {
@@ -10,6 +12,8 @@ extension DatabaseModelsEnumExtension on DatabaseModelsEnum {
     switch (this) {
       case DatabaseModelsEnum.user:
         return 'userBox';
+      case DatabaseModelsEnum.dietaryUserPreferences:
+        return 'dietaryUserPreferencesBox';
       default:
         throw Exception('Invalid enum value');
     }
@@ -19,17 +23,19 @@ extension DatabaseModelsEnumExtension on DatabaseModelsEnum {
     switch (this) {
       case DatabaseModelsEnum.user:
         return true;
+      case DatabaseModelsEnum.dietaryUserPreferences:
+        return true;
       default:
         throw Exception('Invalid enum value');
     }
   }
 
-  Future<Box<dynamic>> get box async{
+  Future<Box<dynamic>> get box async {
     return await Hive.openBox(boxName);
   }
 
   List<String> get keysEnum {
-    switch (this){
+    switch (this) {
       case DatabaseModelsEnum.user:
         return [
           UserModelKeysEnum.id.key, 
@@ -37,8 +43,19 @@ extension DatabaseModelsEnumExtension on DatabaseModelsEnum {
           UserModelKeysEnum.email.key,
           UserModelKeysEnum.profilePicture.key
         ].toList();
+      case DatabaseModelsEnum.dietaryUserPreferences:
+        return [
+          DietarySurveyUserPreferencesKeysEnum.dietaryRestrictions.key,
+          DietarySurveyUserPreferencesKeysEnum.allergies.key,
+          DietarySurveyUserPreferencesKeysEnum.cuisinePreferences.key,
+          DietarySurveyUserPreferencesKeysEnum.mealPreferences.key,
+          DietarySurveyUserPreferencesKeysEnum.favoriteIngredients.key,
+          DietarySurveyUserPreferencesKeysEnum.dislikedIngredients.key,
+          DietarySurveyUserPreferencesKeysEnum.mealsPerDay.key,
+          DietarySurveyUserPreferencesKeysEnum.eatingHabits.key
+        ].toList();
       default:
-       throw Exception('Invalid Enum Value!');
+        throw Exception('Invalid Enum Value!');
     }
   }
 }
