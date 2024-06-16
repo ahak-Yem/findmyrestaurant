5342f2a9ae2629d1bd653ae01bb5550f023607bf
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-06-16 02:30:20 +0200

    Add MultipleChoiceQuestion component

---
+++ lib/src/components/question widgets/multiple_choice_question.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:findmyrestaurant/src/models/dietary_survey_questions_model.dart';
+
+class MultipleChoiceQuestion extends StatefulWidget {
+  final DietarySurveyQuestionsModel question;
+  final Function(List<String>) onNext;
+
+  const MultipleChoiceQuestion({
+    super.key,
+    required this.question,
+    required this.onNext,
+  });
+
+  @override
+  State<MultipleChoiceQuestion> createState() => _MultipleChoiceQuestionState();
+}
+
+class _MultipleChoiceQuestionState extends State<MultipleChoiceQuestion> {
+  final List<String> _selectedOptions = List.empty(growable: true);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: widget.question.options!.map((option) {
+        return CheckboxListTile(
+          title: Text(option),
+          value: _selectedOptions.contains(option),
+          onChanged: (bool? value) {
+            setState(() {
+              if (value == true) {
+                _selectedOptions.add(option);
+              } else {
+                _selectedOptions.remove(option);
+              }
+            });
+          },
+        );
+      }).toList(),
+    );
+  }
+}
