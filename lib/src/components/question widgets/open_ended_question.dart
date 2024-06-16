05efd42b7b25af5c692a3bf1fbc9b0922dcac3f5
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-06-16 02:31:02 +0200

    Add OpenEndedQuestion component

---
+++ lib/src/components/question widgets/open_ended_question.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:findmyrestaurant/src/models/dietary_survey_questions_model.dart';
+
+class OpenEndedQuestion extends StatelessWidget {
+  final DietarySurveyQuestionsModel question;
+  final Function(String) onNext;
+
+  const OpenEndedQuestion({
+    super.key,
+    required this.question,
+    required this.onNext,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController controller = TextEditingController();
+
+    return TextField(
+      controller: controller,
+      decoration: const InputDecoration(labelText: 'Your answer'),
+      onSubmitted: (value) {
+        onNext(value);
+      },
+    );
+  }
+}
