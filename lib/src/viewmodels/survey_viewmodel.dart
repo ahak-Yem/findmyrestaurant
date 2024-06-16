bf14087a182575492fe8770a58e2116e7f26b111
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-06-16 02:12:02 +0200

    Add ViewModel for survey view

---
+++ lib/src/viewmodels/survey_viewmodel.dart
@@ -0,0 +1,46 @@
+import 'package:findmyrestaurant/src/enums/database%20enums/database_models_enum.dart';
+import 'package:findmyrestaurant/src/services/app_launch_service.dart';
+import 'package:findmyrestaurant/src/services/database_service.dart';
+import 'package:findmyrestaurant/src/services/survey_service.dart';
+import 'package:flutter/material.dart';
+import 'package:findmyrestaurant/src/models/dietary_survey_questions_model.dart';
+import 'package:findmyrestaurant/src/models/dietary_survey_user_preferences_model.dart';
+
+class SurveyViewModel extends ChangeNotifier {
+  List<DietarySurveyQuestionsModel> _surveyQuestions = List.empty();
+  List<DietarySurveyQuestionsModel> get surveyQuestions => _surveyQuestions;
+  
+  final DietarySurveyUserPreferencesModel _surveyUserPreferences = DietarySurveyUserPreferencesModel();
+  DietarySurveyUserPreferencesModel get surveyUserPreferences => _surveyUserPreferences;
+
+  String? _userID;
+  
+  final SurveyService _surveyService = SurveyService.instance;
+  final DatabaseService _databaseService = DatabaseService.instance;
+  final AppLaunchService _appLaunchService = AppLaunchService.instance;
+
+  SurveyViewModel() {
+    _surveyQuestions = _surveyService.surveyQuestions;
+    _userID = _appLaunchService.userId;
+  }
+
+  void updateUserResponse(String questionId, dynamic response) {
+    final questionIndex = _surveyQuestions.indexWhere((q) => q.id == questionId);
+    if (questionIndex >= 0) {
+      _surveyUserPreferences.setPreferencesField(questionId, response);
+      notifyListeners();
+    }
+  }
+
+  void saveUserPreferences() async{
+    if(_userID == null){
+      return;  
+    }
+    try{
+      _databaseService.save(DatabaseModelsEnum.dietaryUserPreferences, _userID, _surveyUserPreferences);
+    }
+    catch(e){
+      return;
+    }
+  }
+}
