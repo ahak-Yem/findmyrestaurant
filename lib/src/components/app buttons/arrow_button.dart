bbfbb5192813f10b328ec257160160b5f7e0c154
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-06-22 02:02:24 +0200

    Add a dynamic ArrowButton component.

---
+++ lib/src/components/app buttons/arrow_button.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class ArrowButton extends StatelessWidget {
+  final ArrowDirection direction;
+  final VoidCallback onPressed;
+  final Color containerColor;
+
+  const ArrowButton({
+    Key? key,
+    required this.direction,
+    required this.containerColor,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: containerColor,
+      ),
+      child: IconButton(
+        icon: Transform.rotate(
+          angle: direction.angle,
+          child: Image.asset('assets/icons/arrow_primary.png'),
+        ),
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
+
+enum ArrowDirection {
+  right,
+  up,
+  left,
+  down,
+}
+
+extension ArrowDirectionExtension on ArrowDirection {
+  double get angle {
+    switch (this) {
+      case ArrowDirection.right:
+        return 0;
+      case ArrowDirection.up:
+        return 270 * 3.1415926535 / 180;
+      case ArrowDirection.left:
+        return 180 * 3.1415926535 / 180;
+      case ArrowDirection.down:
+        return 90 * 3.1415926535 / 180;
+      default:
+        return 0;
+    }
+  }
+}
