6516a6ca6c3538b4fc0e809f868ecc608cbfd80f
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-06-22 03:19:41 +0200

    Add SurveyResponseUtil static class to handle questions widgets responses.

---
+++ lib/src/utility/survey_response_util.dart
@@ -0,0 +1,13 @@
+
+class SurveyResponseUtil {
+  static final Map<String, dynamic> _surveyUserPreferences = {};
+  static Map<String, dynamic> get surveyUserPreferences => _surveyUserPreferences;
+
+  static void setResponse(String id, dynamic response) {
+    _surveyUserPreferences[id] = response;
+  }
+
+  static dynamic getResponse(String id) {
+    return _surveyUserPreferences[id];
+  }
+}
