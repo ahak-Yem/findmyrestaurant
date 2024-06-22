f0629d2f76b580393cea383b3037b6ca43a930f9
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-06-23 01:32:51 +0200

    Added images map property to DietarySurveyQuestionsModel and updated romJson method to parse it.

--- lib/src/models/dietary_survey_questions_model.dart
+++ lib/src/models/dietary_survey_questions_model.dart
@@ -5,12 +5,14 @@ class DietarySurveyQuestionsModel {
   final String question;
   final SurveyQuestionType type;
   final List<String>? options;
+  final Map<String, String>? images;
 
   DietarySurveyQuestionsModel({
     required this.id,
     required this.question,
     required this.type,
     this.options,
+    this.images
   });
 
   static DietarySurveyQuestionsModel fromJson(Map<String, dynamic> json) {
@@ -19,6 +21,7 @@ class DietarySurveyQuestionsModel {
       question: json['question'] ?? '',
       type: SurveyQuestionType.values.firstWhere((e) => e.name == json['type'], orElse: () => SurveyQuestionType.openEnded,),
       options: json['options'] != null ? List<String>.from(json['options']) : null,
+      images: json['images'] != null ? Map<String, String>.from(json['images']) : null,
     );
   }
 }
