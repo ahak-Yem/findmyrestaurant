c0e2420e348a6b70d31cddd537f85610b652b31c
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-06-22 02:27:52 +0200

    Added showArrows bool property to AppCarousel to show arrow buttons next to PageIndicator and pass callbacks of the buttons.

--- lib/src/components/app_carousel.dart
+++ lib/src/components/app_carousel.dart
@@ -1,4 +1,6 @@
+import 'package:findmyrestaurant/src/components/app%20buttons/arrow_button.dart';
 import 'package:findmyrestaurant/src/components/dot_page_indicator.dart';
+import 'package:findmyrestaurant/styles/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class AppCarousel extends StatefulWidget {
@@ -7,6 +9,9 @@ class AppCarousel extends StatefulWidget {
   final double heightPercentage;
   final Function(int) onPageChanged;
   final bool isNotScrollable;
+  final bool showArrows;
+  final VoidCallback? onNext;
+  final VoidCallback? onBack;
 
   const AppCarousel({
     Key? key,
@@ -15,6 +20,9 @@ class AppCarousel extends StatefulWidget {
     required this.heightPercentage,
     required this.onPageChanged,
     required this.isNotScrollable,
+    this.showArrows = false,
+    this.onNext,
+    this.onBack,
   }) : super(key: key);
 
   @override
@@ -47,9 +55,28 @@ class _AppCarouselState extends State<AppCarousel> {
         ),
         Container(
           padding: const EdgeInsets.fromLTRB(0, 15, 0, 0),
-          child: PageIndicator(
-            currentPage: _currentIndex,
-            pageCount: widget.items.length,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (widget.showArrows)...[
+                ArrowButton(
+                  direction: ArrowDirection.left,
+                  containerColor: AppColors.primaryColor,
+                  onPressed: _currentIndex > 0 && widget.onBack != null ? widget.onBack! : (){},
+                ),
+              ],
+              PageIndicator(
+                currentPage: _currentIndex,
+                pageCount: widget.items.length,
+              ),
+              if (widget.showArrows)...[
+                ArrowButton(
+                  direction: ArrowDirection.right,
+                  containerColor: AppColors.primaryColor,
+                  onPressed: _currentIndex < widget.items.length - 1 && widget.onBack != null ? widget.onNext! : (){},
+                ),
+              ],
+            ],
           ),
         ),
       ],
