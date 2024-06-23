9bf8c20afc7686d7c5cef0d78bf0ee81c74665a4
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-06-23 21:44:09 +0200

    Use ImageRadioButtonCard with image path from question images or placeholder image, and removed conditional RadioListTile.

--- lib/src/components/question widgets/single_choice_question.dart
+++ lib/src/components/question widgets/single_choice_question.dart
@@ -1,4 +1,7 @@
 import 'package:findmyrestaurant/src/components/cards/image_radiobutton_card.dart';
+import 'package:findmyrestaurant/src/enums/images%20enums/images_names.dart';
+import 'package:findmyrestaurant/src/enums/images%20enums/images_paths_sections_enum.dart';
+import 'package:findmyrestaurant/src/services/images_reader_service.dart';
 import 'package:findmyrestaurant/src/utility/question_widgets_answers_util.dart';
 import 'package:flutter/material.dart';
 import 'package:findmyrestaurant/src/models/dietary_survey_questions_model.dart';
@@ -33,32 +36,20 @@ class _SingleChoiceQuestionState extends State<SingleChoiceQuestion> {
   @override
   Widget build(BuildContext context) {
     final options = widget.question.options ?? [];
+    final images =  widget.question.images ?? {};
 
     return Expanded(
       child: ListView.builder(
         itemCount: options.length,
         itemBuilder: (context, index) {
           final option = options[index];
-          return widget.question.options?.length == widget.question.images?.length ? 
-          ImageRadioButtonCard(
+          return ImageRadioButtonCard(
             label: option,
-            imagePath: widget.question.images![option]!,
+            imagePath: images[option] ?? ImagesReaderService.instance.getImagePath(ImagesPathsSections.extra, ImagesNames.noImagePlaceholder),
             isSelected: _selectedOption == option,
             onSelected: (isSelected) {
               setState(() {
                 _selectedOption = isSelected ? option : "";
-                QuestionWidgetsAnswersUtil.setResponse(widget.question.id, _selectedOption);
-                QuestionWidgetsAnswersUtil.notifyIsQuestionAnswered(widget.question.id, _selectedOption.isNotEmpty);
-              });
-            },
-          ) : 
-          RadioListTile<String>(
-            title: Text(option),
-            value: option,
-            groupValue: _selectedOption,
-            onChanged: (String? value) {
-              setState(() {
-                _selectedOption = value ?? "";
                 QuestionWidgetsAnswersUtil.setResponse(widget.question.id, _selectedOption);
                 QuestionWidgetsAnswersUtil.notifyIsQuestionAnswered(widget.question.id, _selectedOption.isNotEmpty);
               });
