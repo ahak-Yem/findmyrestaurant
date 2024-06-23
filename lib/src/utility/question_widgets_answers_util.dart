9722450ada9ed075c83824861ba9e9fef6972b47
ahak-Yem <ahmedhaniyemen@gmail.com>
2024-06-23 15:55:08 +0200

    Add isQuestionAnswered stream and a notify method with question id and bool value.

--- lib/src/utility/question_widgets_answers_util.dart
+++ lib/src/utility/question_widgets_answers_util.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
 class QuestionWidgetsAnswersUtil {
   static final Map<String, dynamic> _surveyUserPreferences = {};
   static Map<String, dynamic> get surveyUserPreferences => _surveyUserPreferences;
+
+  static final StreamController<Map<String, bool>> _isQuestionAnsweredStream = StreamController<Map<String, bool>>.broadcast();
+  static Stream<Map<String, bool>> get isQuestionAnsweredStream => _isQuestionAnsweredStream.stream;
 
   static void setResponse(String id, dynamic response) {
     _surveyUserPreferences[id] = response;
@@ -8,5 +13,9 @@ class QuestionWidgetsAnswersUtil {
 
   static dynamic getResponse(String id) {
     return _surveyUserPreferences[id];
+  }
+
+  static void notifyIsQuestionAnswered(String id, bool isValueSet) {
+    _isQuestionAnsweredStream.add(Map.from({id : isValueSet}));
   }
 }
